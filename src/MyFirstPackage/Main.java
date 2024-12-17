6a675984c109262924ddf2bdce7f42a931b80f74
Vidko Videv <vidko33@live.com>
2024-12-17 20:59:56 +0200

    Init commit

---
+++ src/MyFirstPackage/Main.java
@@ -0,0 +1,157 @@
+package MyFirstPackage;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //dataTypes();
+        //enterText();
+        //exerciseOne();
+        //exerciseTwo();
+        //exercise3();
+        //exercise4();
+        //exercise5();
+        System.out.println("Enter the number of seconds you would like to convert: ");
+        Scanner scanner = new Scanner(System.in);
+        long seconds = scanner.nextLong();
+
+        long sumOfSecondsInDays = seconds/86400;
+        //long sumOfSecondsInYears = seconds / (60 * 60 * 24 * 365);
+        //long sumOfSecondsInYears = sumOfSecondsInDays/365;
+
+        System.out.println("Seconds in years: " + sumOfSecondsInDays/365);
+        System.out.println("Seconds in days: " + sumOfSecondsInDays);
+    }
+    private static void exercise5(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side A with numbers only:");
+        int sideA = scanner.nextInt();
+
+        System.out.println("Enter side B with numbers only:");
+        int sideB = scanner.nextByte();
+// to check why
+        System.out.println("Enter side C with numbers only:");
+//        String sideCRaw = scanner.nextLine();
+//        int sideC = Integer.parseInt(sideCRaw);
+
+        int sideC = scanner.nextInt();
+        //int sideCv1 = Integer.parseInt(scanner.nextLine());
+
+        int perim = sideA + sideB + sideC;
+        System.out.println("This is perimeter: " + perim);
+    }
+    private static void exercise4(){
+        System.out.println("Here is your pine tree on 2 lines: ");
+        System.out.println(" " + "*" + " ");
+        System.out.println("*" + "*" + "*");
+        System.out.println(" " + "|" + " ");
+
+        System.out.println("Here is your pine tree on 3 lines: ");
+        System.out.println( " " + " " + "*" + " " + " " );
+        System.out.println(" " + "*" + "*" + "*");
+        System.out.println("*" + "*" + "*" + "*" + "*");
+    }
+    private static void enterText(){
+        System.out.println("Enter text:");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("This is scanned text: " + text);
+        int number = scanner.nextInt();
+        System.out.println("This is scanned number: " + number);
+    }
+    private  static void exercise3(){
+        int sideA = 4;
+        int sideB = 6;
+        int area = sideA * sideB;
+        System.out.println("The area is: " + area);
+    }
+    private static void  exerciseTwo(){
+        int sideA = 3;
+        int sideB = 5;
+        int sideC = 2;
+        int perim = sideA + sideB + sideC;
+        System.out.println("This is perimeter: " + perim);
+    }
+    private static void  exerciseOne(){
+        String firstName = "Mihail";
+        String middleName = "Mihail2";
+        String lastName = "Mihail3";
+        System.out.println(firstName + " " + middleName + " "  + lastName);
+    }
+    private static void dataTypes(){
+                /*
+        System.out.println("Hello World!");
+        System.out.println("My name is Vidko");
+
+        int numberForFirstPrint = 100;
+        System.out.println("My first number is: " + numberForFirstPrint);
+        double firstDouble = 16.4;
+        System.out.println(firstDouble);
+        long firstLongNumber = 3000000000L;
+        System.out.println(firstLongNumber);
+        float firstFloatNumber = 2.5F;
+        System.out.println(firstFloatNumber);
+        char oneChar = '[';
+        System.out.println(oneChar);
+        boolean isBoolean = true;
+        System.out.println(isBoolean);
+        short myFirstShort = 15908;
+        byte myByte = 127;
+        String myFirstWord = "Vidko is in good mood";
+        System.out.println(myFirstShort);
+        System.out.println(myByte);
+        System.out.println(myFirstWord);
+        */
+/*
+        int number1 = 250;
+        double number2 = number1;
+        System.out.println("This is number 1: "+number1);
+        System.out.println("This is number 2: "+number2);
+
+        double number3 = 123.5;
+        int number4 = (int)number3;
+        System.out.println("This is number 3: "+number3);
+        System.out.println("This is number 4: "+number4);
+
+        double number5 = 123.5321523523552;
+        float number6 = (float) number3;
+        System.out.println("This is number 5: "+number5);
+        System.out.println("This is number 6: "+number6);
+
+        String stringNumber = "15.73";
+        double number7 = Double.valueOf(stringNumber);
+        System.out.println("This is string to number:" + number7);
+        String stringNumber2 = "15s";
+        int number8 = Integer.valueOf(stringNumber2);
+        System.out.println("This is string to int:" + number8);
+
+ */
+        /*
+        int n1 = 12;
+        int n2 = 5;
+        int n3 = n1 + n2;
+        double n4 = n1/n2;
+        int n5 = n1*n2;
+        int n6 = n1%n2;
+        int n7 = ++n1; // ++x => x = x + 1
+        int n8 = n1++;
+        int n9 = n7;
+        int n10 = n8;
+        int n11 = --n2;
+        System.out.println("This is number 3:" +n3);
+        System.out.println("This is number 4:" +n4);
+        System.out.println("This is number 5:" +n5);
+        System.out.println("This is number 6:" +n6);
+        System.out.println("This is number 7:" +n7);
+        System.out.println("This is number 8:" +n8);
+        System.out.println("This is number 9:" +n9);
+        System.out.println("This is number 10:" +n10);
+        System.out.println("This is number 11:" +n11);
+
+        System.out.println("This is number 1:" +n1);
+        System.out.println("This is number 2:" +n2);
+*/
+        System.out.println("Please enter text");
+        String inputText = System.console().readLine();
+        System.out.println("This is your text: " + inputText);
+    }
+}
