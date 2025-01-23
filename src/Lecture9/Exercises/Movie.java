f1a4a71ec4556d473f746aa53992bcf4dc31c294
Vidko Videv <vidko33@live.com>
2025-01-23 23:47:20 +0200

    Code of lecture 9

---
+++ src/Lecture9/Exercises/Movie.java
@@ -0,0 +1,50 @@
+package Lecture9.Exercises;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private String title;
+    private ArrayList<String> genre;
+    double rating;
+
+    public Movie(String title, ArrayList<String> genre, double movieRating){
+        this.title = title;
+        this.genre = genre;
+        this.rating = movieRating;
+    }
+
+    public Movie(String title, String genre, double movieRating) {
+        ArrayList<String> gerneList = new ArrayList<>();
+        gerneList.add(genre);
+        this.title = title;
+        this.genre = gerneList;
+        this.rating = movieRating;
+    }
+
+    public void printInfo(){
+        System.out.println("The movie details are:");
+        System.out.println("Movie title: " + this.title);
+        System.out.println("Movie genre: " + this.genre);
+        System.out.println("Movie rating: " + this.rating);
+    }
+
+    public ArrayList<String> addGenre(String gerne){
+        if (this.genre.contains(gerne)){
+            System.out.println("The " + gerne + " already exist.");
+            return this.genre;
+        }
+        this.genre.add(gerne);
+        return this.genre;
+    }
+
+    public ArrayList<String> addGenres(ArrayList<String> gernes){
+        for(String gerne : gernes) {
+            if (this.genre.contains(gerne)){
+                continue;
+            }
+            this.genre.add(gerne);
+        }
+
+        return this.genre;
+    }
+}
